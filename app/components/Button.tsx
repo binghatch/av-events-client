671bdcd2bcd4b23125206a84503a179dd786bbcc
binghatch <aron.seiffert@gmail.com>
2023-07-26 23:09:30 +0200

    fix: remove button code

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,19 +1,19 @@
-import Link from "next/link";
-import { LucideIcon } from "lucide-react";
+// import Link from "next/link";
+// import { LucideIcon } from "lucide-react";
 
-interface ButtonProps {
-  url: string;
-  text: string;
-  icon?: LucideIcon; // Make the icon prop optional by adding "?"
-}
+// interface ButtonProps {
+//   url: string;
+//   text: string;
+//   icon?: LucideIcon; // Make the icon prop optional by adding "?"
+// }
 
-export default function Button(props: ButtonProps) {
-  return (
-    <>
-      <Link href={props.url}>
-        {props.icon && <span><LucideIcon name={props.icon} /></span>}
-        {props.text}
-      </Link>
-    </>
-  );
-}
+// export default function Button(props: ButtonProps) {
+//   return (
+//     <>
+//       <Link href={props.url}>
+//         {props.icon && <span><LucideIcon name={props.icon} /></span>}
+//         {props.text}
+//       </Link>
+//     </>
+//   );
+// }
