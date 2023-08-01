81f23dd30930deec7b7361cc579b75f78821861d
binghatch <aron.seiffert@gmail.com>
2023-08-01 11:16:24 +0200

    style: change logo

--- app/(sections)/NavBarSection.tsx
+++ app/(sections)/NavBarSection.tsx
@@ -1,13 +1,13 @@
 import Link from "next/link";
-import Logo from '../../public/images/av-summit-logo.svg';
+import Logo from '../../public/images/av-logo.svg';
 import Image from 'next/image';
 
 export default function NavBarSection() {
   return (
     <nav className="flex flex-row justify-between items-center w-full pt-8 px-6">
       <div>
-        <Link href={"/"}>
-          <Image src={Logo} height={40} alt="Assembly Summit" unoptimized/>
+        <Link href={"https://www.assemblyventures.com/"} target="_blank">
+          <Image src={Logo} quality={100} height={40} alt="Assembly Summit" unoptimized/>
         </Link>
       </div>
       <div>
