e8da8c9e49f0eb76f2795d0f047c39deda5cef95
binghatch <aron.seiffert@gmail.com>
2023-08-04 01:43:54 +0200

    fix: add imprint

--- app/(sections)/NewsletterSection.tsx
+++ app/(sections)/NewsletterSection.tsx
@@ -5,7 +5,7 @@ export default async function NewsletterSection() {
   return (
     <section className="mt-12 p-6" id="speakers">
       <h2 className=""><span className="thick-underline text-3xl font-black">Subscribe</span></h2>
-      <p className="mt-1">Sign up for our newsletter.</p>
+      <p className="mt-1">Sign up to our newsletter.</p>
       <div className="mt-8">
         <MailchimpForm />
       </div>
--- app/imprint/page.tsx
+++ app/imprint/page.tsx
@@ -9,21 +9,21 @@ export default function Page() {
           <p className='opacity-30'>Information in accordance with Section 5 TMG:</p>
 
           <p className='mt-4'>
-            SampleCompany GmbH<br />
-            SampleStreet 1<br />
-            12345 SampleCity
+            Assembly Ventures Management, LLC<br />
+            1555 Broadway St, 3rd Floor<br />
+            Detroit, MI 48226<br />
+            United States
           </p>
 
           <h3 className='font-bold mt-8 text-sm'>Represented by</h3>
-          <p>Managing Director: Max Sampleman</p>
+          <p>Co-Founders and Partners: Jessica Robinson and Chris Thomas</p>
 
           <h3 className='font-bold mt-8 text-sm'>Contact</h3>
           <p>
-            Telephone: +49 123 456789<br />
-            Fax: +49 123 45678910<br />
-            Email: info@samplecompany.de
+            Telephone: +1 (313) 277-9837<br />
+            Email: contact@assemblyventures.com
           </p>
-
+{/* 
           <h3 className='font-bold mt-8 text-sm'>Register entry</h3>
           <p>
             Entry in the Handelsregister.<br />
@@ -34,13 +34,13 @@ export default function Page() {
           <h3 className='font-bold mt-8 text-sm'>VAT ID</h3>
           <p>
             VAT indentification number in accorance with Section 27a of the German VAT act: DE123456789
-          </p>
+          </p> */}
 
           <h3 className='font-bold mt-8 text-sm'>Person responsible for content in accordance with 55 Abs. 2 RStV</h3>
           <p>
-            Max Sampleman<br />
-            SampleStreet 1<br />
-            12345 SampleCity
+            Felix Scheuffelen<br />
+            Heinrich-Roller-Straße 16B<br />
+            10405 Berlin
           </p>
         </section>
       </main>
