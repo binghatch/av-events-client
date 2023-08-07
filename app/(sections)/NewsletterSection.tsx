e26a1778f0e2929111fb3e0867fda7293e8c056f
binghatch <aron.seiffert@gmail.com>
2023-08-07 12:14:45 +0200

    chore: fix typos

--- app/(sections)/NewsletterSection.tsx
+++ app/(sections)/NewsletterSection.tsx
@@ -5,7 +5,7 @@ export default async function NewsletterSection() {
   return (
     <section className="mt-12 px-4 py-6" id="speakers">
       <h2 className=""><span className="thick-underline text-3xl font-black">Subscribe</span></h2>
-      <p className="mt-1">Sign up to our newsletter.</p>
+      <p className="mt-1">Join our Mailing List.</p>
       <div className="mt-8">
         <MailchimpForm />
       </div>
