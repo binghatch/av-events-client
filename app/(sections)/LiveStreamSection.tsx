6d763fb5278f71946db043d89e98611e5e92f437
binghatch <aron.seiffert@gmail.com>
2023-09-06 10:28:47 +0200

    fix: change livestream link

--- app/(sections)/LiveStreamSection.tsx
+++ app/(sections)/LiveStreamSection.tsx
@@ -9,7 +9,7 @@ export default function LivestreamSection() {
     <section className="px-4 pb-6" id="sessions">
       <h2 className=""><span className="thick-underline text-3xl font-black">Livestream</span></h2>
       <p className="mt-1">Click on the player below to watch our livestream.</p>
-      <ReactPlayer url='https://youtube.com/watch?v=rg602tcvq4c' width='100%' height='100%'className="w-full aspect-video mt-6"/>
+      <ReactPlayer url='https://www.youtube.com/watch?v=CDjVVBL95io' width='100%' height='100%'className="w-full aspect-video mt-6"/>
     </section>
   )
 }
